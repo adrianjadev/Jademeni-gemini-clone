76335a90d52b62f4d5a1c6be46db4ba4c3509799
Adrian Jade Manaday <adrianjademanaday@gmail.com>
2024-11-01 13:42:10 +0800

    Creating the backend functionality for using the Gemini API and making AI generative results

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,50 +1,83 @@
-import React from 'react'
+import React, { useContext }from 'react'
 import './Main.css'
 import { assets } from '../../assets/assets'
+import { Context } from '../../context/Context'
 
 const Main = () => {
+
+    const { onSent, recentPrompt, showResult, loading, resultData, setInput, input } = useContext(Context);
+    
   return (
     <div className='main'>
         <div className="nav">
             <p>Jade-meni</p>
-            <img src={assets.user_icon} alt="" />
+            <img src={assets.jademeni_icon} alt="" />
+            {/* <img src={assets.user_icon} alt="" /> */}
         </div>
         <div className="main-container">
-            <div className="greet">
-                <p><span>Hello, Jade.</span></p>
-                <p>How can I help you today?</p>
+
+            {!showResult
+            ?<>
+                <div className="greet">
+                    <p><span>Hello, I'm Jade-meni!</span></p>
+                    <p>How can I help you today?</p>
+                </div>
+                <div className="cards">
+                    <div className="card">
+                        <p>Suggest beautiful places to see on an upcoming road trip</p>
+                        <img src={assets.compass_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Briefly summarize this concept: urband planning</p>
+                        <img src={assets.bulb_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Brainstorm team building activities for our work retreat</p>
+                        <img src={assets.message_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Improve the readability of the following code</p>
+                        <img src={assets.code_icon} alt="" />
+                    </div>
+                </div>
+            </>
+            :
+            <div className='result'>
+                <div className="result-title">
+                    <img src={assets.user_icon} alt="" />
+                    <p>{recentPrompt}</p>
+                </div>
+                <div className="result-data">
+                    <img src={assets.jademeni_icon} alt="" />
+                    {loading 
+                    ?<div className='loader'>
+                        <hr />
+                        <hr />
+                        <hr />
+                    </div>
+                    :<p dangerouslySetInnerHTML={{__html:resultData}}></p> 
+                    }
+
+                </div>
             </div>
-            <div className="cards">
-                <div className="card">
-                    <p>Suggest beautiful places to see on an upcoming road trip</p>
-                    <img src={assets.compass_icon} alt="" />
-                </div>
-                <div className="card">
-                    <p>Briefly summarize this concept: urband planning</p>
-                    <img src={assets.bulb_icon} alt="" />
-                </div>
-                <div className="card">
-                    <p>Brainstorm team building activities for our work retreat</p>
-                    <img src={assets.message_icon} alt="" />
-                </div>
-                <div className="card">
-                    <p>Improve the readability of the following code</p>
-                    <img src={assets.code_icon} alt="" />
-                </div>
-            </div>
+            }
+
 
             {/* main bottom */}
             <div className="main-bottom">
                 <div className="search-box">
-                    <input type="text" placeholder='Enter a prompt here'/>
+                    <input onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder='Enter a prompt here'/>
                     <div className="">
                         <img src={assets.gallery_icon} alt="" />
                         <img src={assets.mic_icon} alt="" />
-                        <img src={assets.send_icon} alt="" />
+                        {input
+                        ?<img onClick={() => onSent()} src={assets.send_icon} alt="" />
+                        :null
+                        }
                     </div>
                 </div>
                 <p className="bottom-info">
-                    Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps
+                    Jade-meni may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps
                 </p>
             </div>
         </div>
